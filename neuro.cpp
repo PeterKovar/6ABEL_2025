c690f3f8f9b90103cb316909ed0383e77d8b3bbc
Peter Kovar <kova@htl-donaustadt.at>
2025-03-08 07:30:20 +0100

    .gitignore dazu gefuegt

--- neuro.cpp
+++ neuro.cpp
@@ -43,13 +43,48 @@ class NeuronNet {
         this->numLayers = numLayers;
         //Layer erzeugen
         layers = new Layer[this->numLayers];
+        //Neuronen in den einzelnen Layer erzeugen
+       for (int i=0; i<numLayers; ++i) {
+        layers[i].numNeurons = layer_struct[i]; //Anzahl Neuronen über übernehmen
+        layers[i].makeNeurons(); //Neuronen erzeugen
+        if (i==0) layers[i].numInputs = 0; //Layer 0 kein Input
+        else layers[i].numInputs = layers[i-1].numNeurons;  // Anz. Inputs =
+       }                                                    // Anz. Neur. im vorigen Layer
         
+    }
+    // Destruktor
+    ~NeuronNet() {
+        delete [] layers; //Speicher wieder freigeben
+    }
+
+    //Vorwaertsfunktion
+    void forward (double *input) {
+        // Setzen der Input-Werte in den Eingangslayer
+        for (int i=0;i<layers[0].numNeurons; ++i){
+          layers[0].neurons[i].output = input[i];
+        }
+        //Berechnen der Ausgaben für jede Schicht
+        for(int i=1; i < numLayers; ++i) {     // Alle Layer durchgehen, Eingangslayer bereits erledigt
+          for(int j=0; j<layers[i].numNeurons; ++j){  // Alle Neuronen durchgehen
+            double sum = 0.0;                         // Ausgaenge summieren
+            for (int k=0; k < layers[i].numInputs; ++k) {   //Alle Eingaenge durchgehen
+              //Alle Eingaenge mit dem Gewicht mult. und summieren
+              sum += layers[i-1].neurons[k].output *
+                     layers[i].neurons[j].weight;
+            }
+            layers[i].neurons[j].output = sigmoid(sum); // Mit Aktivierungsfunktion norm.
+          }
+        }
     }
 };
 
 int main(int argc, char **argv){
-    int layer_struct[] = {2, 3, 1};
-    NeuronNet net(layer_struct, sizeof(layer_struct)/sizeof(int));
+    int layer_struct[] = {2, 3, 1};  // Struktur des Netzwerkes festlegen
+    NeuronNet net(layer_struct, sizeof(layer_struct)/sizeof(int)); // net instanzieren
+    double input[] = {0.5, 0.8};
+    // Ausgang berechnen
+    net.forward(input);
+    std::cout << net.layers[2].neurons[0].output;
 
  return 0;   
 }
