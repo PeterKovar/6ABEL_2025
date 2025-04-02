71d74483919525f7a5f411054a60674c1315556b
PeterKovar <kova@htl-donaustadt.at>
2025-04-02 18:07:50 +0200

    Update neuro.cpp

--- neuro.cpp
+++ neuro.cpp
@@ -1,89 +1,125 @@
-// Proj. Neuronales Netz v1.0
-//(c) kova 26.02.2025
-
 # include <iostream>
 # include <cmath>
 
-double sigmoid(double s){
-    return 1.0 / (1.0 + exp(-s));   
+double sigmoid(double s) {
+    return 1.0 / (1.0 + exp(-s));
 }
-double sigmoid_derivative(double s){  // Sigmoid Wert muss uebergeben werden
-    return s * (1.0 -s);
+
+double sigmoid_derivative(double s) {
+    return s * (1.0 - s); 
 }
-//Neuronenklasse
-class Neuron{
-    public:
+
+// Neuronenklasse
+class Neuron {
+public:
     double weight;
+    double bias;
     double output;
-    //Konstruktor
-    Neuron() : weight(0.5), output(0.0) {}    
+    double delta;
+
+    Neuron() : weight(((double) rand() / RAND_MAX) * 2 - 1), // Zufällige Initialisierung zwischen -1 und 1
+               bias(((double) rand() / RAND_MAX) * 2 - 1),
+               output(0.0), delta(0.0) {}
 };
-//Layerklasse
-class Layer{
-    public:
+
+// Layerklasse
+class Layer {
+public:
     Neuron* neurons;
     int numNeurons;
     int numInputs;
-    //Konstruktor
+    //double* inputs;
+
     Layer() {}
-    void makeNeurons(){
-        neurons = new Neuron[numNeurons];
+
+    void makeNeurons() {
+        neurons = new Neuron[numNeurons]; // Neuronen erzeugen
     }
-    ~Layer(){
-        delete[] neurons;  //Speicher freigeben
+
+    ~Layer() {
+        delete[] neurons; // Speicher freigeben
     }
 };
-//Neuronales Netz
+
 class NeuronNet {
-    public:
+public:
     Layer* layers;
     int numLayers;
-    //Konstruktor
-    NeuronNet(int *layer_struct, int numLayers) {
-        this->numLayers = numLayers;
-        //Layer erzeugen
-        layers = new Layer[this->numLayers];
-        //Neuronen in den einzelnen Layer erzeugen
-       for (int i=0; i<numLayers; ++i) {
-        layers[i].numNeurons = layer_struct[i]; //Anzahl Neuronen über übernehmen
-        layers[i].makeNeurons(); //Neuronen erzeugen
-        if (i==0) layers[i].numInputs = 0; //Layer 0 kein Input
-        else layers[i].numInputs = layers[i-1].numNeurons;  // Anz. Inputs =
-       }                                                    // Anz. Neur. im vorigen Layer
-        
-    }
-    // Destruktor
-    ~NeuronNet() {
-        delete [] layers; //Speicher wieder freigeben
+    NeuronNet(int* layer_struc, int maxLayer) {
+        numLayers = maxLayer;
+        layers = new Layer[numLayers];
+
+        for (int i = 0; i < numLayers; ++i) {
+            layers[i].numNeurons = layer_struc[i];
+            layers[i].makeNeurons();
+            if (i == 0) layers[i].numInputs = 0;
+            else layers[i].numInputs = layers[i - 1].numNeurons;
+        }
     }
 
-    //Vorwaertsfunktion
-    void forward (double *input) {
-        // Setzen der Input-Werte in den Eingangslayer
-        for (int i=0;i<layers[0].numNeurons; ++i){
-          layers[0].neurons[i].output = input[i];
+    ~NeuronNet() {
+        delete[] layers;
+    }
+
+    // Vorwärtsfunktion
+    void forward(double* input) {
+        for (int i = 0; i < layers[0].numNeurons; ++i) {
+            layers[0].neurons[i].output = input[i];
         }
-        //Berechnen der Ausgaben für jede Schicht
-        for(int i=1; i < numLayers; ++i) {     // Alle Layer durchgehen, Eingangslayer bereits erledigt
-          for(int j=0; j<layers[i].numNeurons; ++j){  // Alle Neuronen durchgehen
-            double sum = 0.0;                         // Ausgaenge summieren
-            for (int k=0; k < layers[i].numInputs; ++k) {   //Alle Eingaenge durchgehen
-              //Alle Eingaenge mit dem Gewicht mult. und summieren
-              sum += layers[i-1].neurons[k].output *
-                     layers[i].neurons[j].weight;
+
+        for (int i = 1; i < numLayers; ++i) {
+            for (int j = 0; j < layers[i].numNeurons; ++j) {
+                double sum = 0.0;
+                for (int k = 0; k < layers[i].numInputs; ++k) {
+                    sum += layers[i - 1].neurons[k].output * layers[i].neurons[j].weight;
+                }
+                sum += layers[i].neurons[j].bias; // Bias hinzufügen
+                layers[i].neurons[j].output = sigmoid(sum);
             }
-            layers[i].neurons[j].output = sigmoid(sum); // Mit Aktivierungsfunktion norm.
-          }
+        }
+    }
+
+    // Backpropagation: Training mit dem Lernfaktor
+    void train(double* input, double* target, double learning_rate) {
+        // Durchlauf
+        forward(input);
+
+        // Fehlerberechnung für den Output-Layer
+        for (int i = 0; i < layers[numLayers - 1].numNeurons; ++i) {
+            double error =  target[i] - layers[numLayers - 1].neurons[i].output;
+            layers[numLayers - 1].neurons[i].delta = error * sigmoid_derivative(layers[numLayers - 1].neurons[i].output);
+        }
+
+        // Fehler zurückpropagieren (Backpropagation)
+        for (int i = numLayers - 2; i > 0; --i) {
+            for (int j = 0; j < layers[i].numNeurons; ++j) {
+                double error = 0.0;
+                for (int k = 0; k < layers[i + 1].numNeurons; ++k) {
+                    error += layers[i + 1].neurons[k].delta * layers[i + 1].neurons[k].weight;
+                }
+                layers[i].neurons[j].delta = error * sigmoid_derivative(layers[i].neurons[j].output);
+            }
+        }
+
+        // Gewichte und Biases aktualisieren
+        for (int i = 1; i < numLayers; ++i) {
+            for (int j = 0; j < layers[i].numNeurons; ++j) {
+                for (int k = 0; k < layers[i].numInputs; ++k) {
+                    layers[i].neurons[j].weight += learning_rate * layers[i].neurons[j].delta * layers[i - 1].neurons[k].output;
+                }
+                layers[i].neurons[j].bias += learning_rate * layers[i].neurons[j].delta;
+            }
         }
     }
 };
 
+// Hauptprogramm
 int main() {
-    // Netzwerkstruktur
-    int layer_struc[] = {2, 3, 5, 1};
+    //Struktur des Netzes Festlegen Anzahl der Layer mit Anz. Neuronen
+    int layer_struc[] = {2, 3, 1};
     NeuronNet net(layer_struc, sizeof(layer_struc) / sizeof(int));
 
-    // UND-Trainingsdaten
+    // Trainingsdaten für UND-Funktion
     double inputs[4][2] = {
         {0, 0},
         {0, 1},
@@ -93,32 +129,39 @@ int main() {
 
     double targets[4][1] = {
         {0},
-        {0},
         {1},
-        {0}
+        {1},
+        {1}
     };
 
     double learning_rate = 0.5;
     int epochs = 5000;
 
-    // Training
+    // Training des Netzwerks
     for (int epoch = 0; epoch < epochs; ++epoch) {
         double total_error = 0.0;
         for (int i = 0; i < 4; ++i) {
+            //Uebergabe der Zeilenzeiger von inputs und targets
             net.train(inputs[i], targets[i], learning_rate);
+
+            // Fehler berechnen
             net.forward(inputs[i]);
-            double error = pow(targets[i][0] - net.layers[net.numLayers - 1].neurons[0].output, 2);
+            double error = pow(targets[i][0] - net.layers[sizeof(layer_struc)/sizeof(int)-1].neurons[0].output, 2);
             total_error += error;
         }
+        // Nach 500 Iterationen Zwischenergebnis des Fehlers ausgeben
         if (epoch % 500 == 0) {
             std::cout << "Epoche " << epoch << " - Fehler: " << total_error / 4.0 << std::endl;
         }
     }
 
-    // Testen
+    // Netz testen
     std::cout << "\nTrainiertes Netz:\n";
     for (int i = 0; i < 4; ++i) {
         net.forward(inputs[i]);
         std::cout << "Eingang: " << inputs[i][0] << ", " << inputs[i][1]
-                  << " -> Ausgabe: " << net.layers[net.numLayers - 1].neurons[0].output << std::endl;
+                  << " -> Ausgabe: " << net.layers[sizeof(layer_struc)/sizeof(int)-1].neurons[0].output << std::endl;
     }
+
+    return 0;
+}
